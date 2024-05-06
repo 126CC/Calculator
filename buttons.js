3be84abac0652ebb815fd18dcb4d54b3b867b48c
126CC <113187762+126CC@users.noreply.github.com>
2024-05-06 12:37:06 -0400

    editing history

--- buttons.js
+++ buttons.js
@@ -1,109 +1,37 @@
 let displayNum = "";
-
-function enterExp() {
-    displayNum += "**";
+let value = "";
+    function displayVal(value) {
+        displayNum += value;
+        document.getElementById("display").innerHTML = displayNum;
+    }
+function deleteNum() {
+    if(displayNum.slice(-2) === "**") {
+        displayNum = displayNum.substring(0, displayNum.length - 2);
+    } else {
+        displayNum = displayNum.substring(0, displayNum.length - 1);
+    }
     document.getElementById("display").innerHTML = displayNum;
-}
-
-function enter7() {
-    displayNum += "7";
-    document.getElementById("display").innerHTML = displayNum;
-}
-
-function enter8() {
-    displayNum += "8";
-    document.getElementById("display").innerHTML = displayNum;
-}
-
-function enter9() {
-    displayNum += "9";
-    document.getElementById("display").innerHTML = displayNum;
-}
-
-function enterMultiply() {
-    displayNum += "*";
-    document.getElementById("display").innerHTML = displayNum;
-}
-
-function enterSqRoot() {
-    displayNum += "√";
-    document.getElementById("display").innerHTML = displayNum;
-}
-
-function enter4() {
-    displayNum += "4";
-    document.getElementById("display").innerHTML = displayNum;
-}
-
-function enter5() {
-    displayNum += "5";
-    document.getElementById("display").innerHTML = displayNum;
-}
-
-function enter6() {
-    displayNum += "6";
-    document.getElementById("display").innerHTML = displayNum;
-}
-
-function enterMinus() {
-    displayNum += "-";
-    document.getElementById("display").innerHTML = displayNum;
-}
-
-function del() {
-
-}
-
-function enter1() {
-    displayNum += "1";
-    document.getElementById("display").innerHTML = displayNum;
-}
-
-function enter2() {
-    displayNum += "2";
-    document.getElementById("display").innerHTML = displayNum;
-}
-
-function enter3() {
-    displayNum += "3";
-    document.getElementById("display").innerHTML = displayNum;
-}
-
-function enterPlus() {
-    displayNum += "+";
-    document.getElementById("display").innerHTML = displayNum;
-}
-
-function clear() {
-    displayNum = "";
-    document.getElementById("display").innerHTML = displayNum;
-}
-
-function enter0() {
-    displayNum += "0";
-    document.getElementById("display").innerHTML = displayNum;
-}
-
-function enterOpenP() {
-    displayNum += "(";
-    document.getElementById("display").innerHTML = displayNum;
-}
-
-function enterCloseP() {
-    displayNum += ")";
-    document.getElementById("display").innerHTML = displayNum;
-}
-
-function enterDivision() {
-    displayNum += "/";
-    document.getElementById("display").innerHTML = displayNum;
-}
-
-function history() {
-
 }
 
 function enterNum() {
     document.getElementById("display").innerHTML = eval(displayNum);
+}
+
+function history() {
+        // localStorage.setItem("equation", displayNum);
+        // JSON.stringify(eval(displayNum));
+        // localStorage.setItem("solution", eval(displayNum));
+    let history =
+        {
+            equation: displayNum,
+            solution: eval(displayNum)
+        }
+        
+    }
+
+
+function clearNum() {
+        displayNum = "";
+    document.getElementById("display").innerHTML = displayNum;
 }
 
