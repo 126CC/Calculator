3206f8a60d2054f57e326a6fcfa5d6b794ed7055
126CC <113187762+126CC@users.noreply.github.com>
2024-05-10 12:29:33 -0400

    x

--- buttons.js
+++ buttons.js
@@ -7,6 +7,15 @@ let value = "";
 function deleteNum() {
     if(displayNum.slice(-2) === "**") {
         displayNum = displayNum.substring(0, displayNum.length - 2);
+    }
+    if(displayNum.slice(-8) === "Math.cos"){
+        displayNum = displayNum.substring(0, displayNum.length - 8);
+    }
+    if(displayNum.slice(-8) === "Math.sin"){
+        displayNum = displayNum.substring(0, displayNum.length - 8);
+    }
+    if(displayNum.slice(-8) === "Math.tan"){
+        displayNum = displayNum.substring(0, displayNum.length - 8);
     } else {
         displayNum = displayNum.substring(0, displayNum.length - 1);
     }
@@ -22,28 +31,38 @@ if(counter == null) {
 function enterNum() {
 
     let eq = displayNum;
-
+    document.getElementById("display").innerHTML = eval(displayNum);
 
         counter++;
         localStorage.setItem(counter, JSON.stringify(eq));
-        localStorage.setItem("counter", counter);
-
-    document.getElementById("display").innerHTML = eval(displayNum);
+        displayNum = "";
 
 }
 
 function history() {
-
-    let eq = [];
-        for(let i = 0; i <= counter; i++) {
-            eq.push(localStorage.getItem(""));
-            }
-            document.getElementById("display").innerHTML = eq;
+    if(counter === 0) {
+        let historyNumDisp = document.createElement("div");
+        historyNumDisp.innerHTML = "No history found.";
+        historyNumDisp.classList.add("historyNumDisp");
+        document.getElementById("history").appendChild(historyNumDisp);
+    } else {
+        let historyNum = JSON.parse(localStorage.getItem(counter));
+        counter--;
+        let historyNumDisp = document.createElement("div");
+        historyNumDisp.innerHTML = historyNum;
+        historyNumDisp.classList.add("historyNumDisp");
+        document.getElementById("history").appendChild(historyNumDisp);
+        displayNum = historyNum;
+        document.getElementById("display").innerHTML = displayNum;
+    }
         }
 
 function clearNum() {
         displayNum = "";
     document.getElementById("display").innerHTML = displayNum;
+    counter = 0;
+    localStorage.clear();
+    location.reload();
 }
 
 function upEq() {
